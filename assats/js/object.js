95399caab999012a12af2eea2fc5351597ee0e26
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-12 21:22:52 +0600

    addad the object

---
+++ assats/js/object.js
@@ -0,0 +1,13 @@
+const object = {
+    name:"sabbir",
+    age: 44,
+    details:{
+        job: "no",
+        isMarrid: true,
+    } 
+}
+// console.log(object.details.isMarrid);
+
+const numbers =[1, 2, 3, 4, 5, 6];
+const newArray =numbers.map(number => number +1);
+console.log(newArray);
