53e053ef2a4c118ebf803148bb5498ade1471893
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-15 21:53:01 +0600

    addad the new function

--- assats/js/object.js
+++ assats/js/object.js
@@ -13,11 +13,16 @@ const newArray = numbers.map(number => number +1);
 // console.log(newArray);
 
 const num1 = 10;
-const num2 = "10";
+const num2 = "10"; 
 
 if(num1 == num2){
     console.log("This Is Wqual Value");
 }
 else{
     console.log("This Is not Equal");
+}
+
+function sum( num1, num2){
+    const sum = num1 + num2;
+    return sum
 }
