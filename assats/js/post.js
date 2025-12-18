750943a5f53fd405359c63c6c6fc50fdbfca944d
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-18 12:11:54 +0600

    addad the design

---
+++ assats/js/post.js
@@ -0,0 +1,25 @@
+const postBtn = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data =>{
+        postAllData(data)
+    })
+}
+
+const postAllData = (users) => {
+    const divContainer = document.getElementById('div-container');
+    for(const user of users){
+        const div = document.createElement("div")
+        div.classList.add("card")
+        div.innerHTML=`
+        <h1>${user.userId}</h1>
+        <h2>${user.id}</h2>
+        <h1>${user.title}</h1>
+        <p>${user.body}</p>`
+
+
+        divContainer.appendChild(div)
+    }
+        
+}
+postBtn ()
