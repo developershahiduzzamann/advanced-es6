24603adc363a263f2edc7866ffac942bfa0d086a
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-12-22 15:57:30 +0600

    addad the all recap

--- assats/js/pech.js
+++ assats/js/pech.js
@@ -1,11 +1,20 @@
-const url = [
-    'https://jsonplaceholder.typicode.com/posts',
-    'https://jsonplaceholder.typicode.com/comments',
-    'https://jsonplaceholder.typicode.com/albums'
-];
-Promise.all(url.map(url =>{
-    return fetch(url).then(res => res.json()).then
-    (data => data)
-}))
-.then(res => res)
-.then(data => console.log(data))
+// const url = [
+//     'https://jsonplaceholder.typicode.com/posts',
+//     'https://jsonplaceholder.typicode.com/comments',
+//     'https://jsonplaceholder.typicode.com/albums'
+// ];
+// Promise.all(url.map(url =>{
+//     return fetch(url).then(res => res.json()).then
+//     (data => data)
+// })) 
+// .then(res => res)
+// .then(data => console.log(data))
+
+console.log("Time Out Function");
+console.log("Time Out Function");
+
+setTimeout( ()=>{
+    console.log('time Out two');
+},3000)
+console.log("Time Out Function");
+console.log("Time Out Function");
